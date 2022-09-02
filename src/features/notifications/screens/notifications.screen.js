101f73b6d41c30fdf4acbbeb3ad5dc1c453bedcc
tushar4303 <padhytushar@protonmail.com>
2022-09-02 12:02:40 +0530

    work on bottom sheet

--- src/features/notifications/screens/notifications.screen.js
+++ src/features/notifications/screens/notifications.screen.js
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import {
   SafeAreaView,
   View,
   StyleSheet,
   StatusBar,
   ScrollView,
+  Text,
 } from "react-native";
 import UIComponent from "../components/notification-info-card.component";
-import { Text as PaperText, Chip, Divider } from "react-native-paper";
+import { Text as PaperText, Chip } from "react-native-paper";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
-export const NotificationsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.title}>
-      <PaperText variant="headlineSmall">Notifications</PaperText>
-    </View>
-    <View style={styles.chip}>
-      <Chip compact="true">Label</Chip>
-      <Chip compact="true" style={{ marginLeft: 8 }}>
-        Unread
-      </Chip>
-      <Chip compact="true" style={{ marginLeft: 8 }}>
-        filter
-      </Chip>
-    </View>
-    <ScrollView style={styles.list}>
-      <UIComponent />
-      <UIComponent />
-      <UIComponent />
-      <UIComponent />
-    </ScrollView>
-  </SafeAreaView>
-);
+export const NotificationsScreen = () => {
+  const sheetRef = useRef < BottomSheet > null;
+  const [isOpen, setIsOpen] = useState(true);
+
+  const snapPoints = ["40%", "60%", "80%"];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <PaperText variant="headlineSmall">Notifications</PaperText>
+      </View>
+      <View style={styles.chip}>
+        <Chip compact="true">Label</Chip>
+        <Chip compact="true" style={{ marginLeft: 8 }}>
+          Unread
+        </Chip>
+        <Chip compact="true" style={{ marginLeft: 8 }}>
+          filter
+        </Chip>
+      </View>
+      <ScrollView style={styles.list}>
+        <UIComponent />
+        <UIComponent />
+        <UIComponent />
+        <UIComponent />
+      </ScrollView>
+      <BottomSheet
+        style={{ backgroundColor: "grey" }}
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+      >
+        <BottomSheetView>
+          <Text>Hello</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: "#FF5A5F",
+    secondary: "#FC642D",
+    muted: "#ff9c9f",
+  },
+  ui: {
+    primary: "#262626",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#FFFFFF",
+    secondary: "#F1F1F1",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "#767676",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
